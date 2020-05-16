8330a2c1af275228a2bbf89164effdff3bfba38b
chiniczR <chiniczrebeca@gmail.com>
2020-05-16 21:55:08 +0300

    Removed diagram

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,14 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_Kuy00BA0H',
-        userPoolClientId: '3mhu84ncp4u2n5tujb6hgq8ks7',
-        region: 'us-west-2'
+        userPoolId: '', // Your user pool ID
+        userPoolClientId: '', // Your user pool client ID
+        region: '' // The region of your Cognito user pool
     },
     api: {
-        invokeUrl: 'https://41wdgxw3i8.execute-api.us-east-2.amazonaws.com/prod'
+        invokeUrl: ''   // Invoke URL for you API Gateway (stage-specific)
     },
     identity: {
-        idPoolId: 'us-east-2:be4d13f7-7fc3-4b9d-b0b1-ab448dd7271b',
-        region: 'us-east-2'
+        idPoolId: '', // Your identity pool ID
+        region: '' // The region of your Cognito identity pool
     }
 };
